69499dc0bac4240d2439503bbe8c2984b26f304e
Eithan Shavit <eithanshavit@gmail.com>
2013-12-31 14:15:25 -0800

    More styling

--- eithanshavit.py
+++ eithanshavit.py
@@ -45,30 +45,30 @@ verbalClockImages = {
 }
 
 
-@app.route('/favicon.ico')
+@app.route( '/favicon.ico' )
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.png' )
+    return send_from_directory( os.path.join( app.root_path, 'static'), 'favicon.png' )
 
-@app.route('/hardware.html')
-@app.route('/hardware')
+@app.route( '/hardware.html' )
+@app.route( '/hardware' )
 def hardware():
    return render_template( 'hardware.html', menu=menu, page='hardware',
    vcImages=verbalClockImages )
 
-@app.route('/<page>')
+@app.route( '/<page>' )
 def page( page='index' ):
    return render_template( page + '.html', menu=menu, page=page )
 
-@app.route('/')
-@app.route('/index.html')
+@app.route( '/' )
+@app.route( '/index.html' )
 def index():
    return render_template( 'about.html', menu=menu, page='about' )
 
 
-@app.errorhandler(404)
-@app.errorhandler(500)
-def page_not_found(error):
-    return "Oops, are you in the right place?"
+@app.errorhandler( 404 )
+@app.errorhandler( 500 )
+def page_not_found( error ):
+   return "Oops, are you in the right place?"
 
 if __name__ == '__main__':
-   run_wsgi_app(app)
+   run_wsgi_app( app )
